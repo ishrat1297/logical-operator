0565827b0739e569c898ac9dd864be8cae0934d6
afshan feroz <160025739+ishrat1297@users.noreply.github.com>
2024-03-24 13:16:24 +0500

    Add files via upload
    Logical Operators
    Logical operators are fundamental components of programming languages that allow you to combine multiple conditions and make decisions based on the result. They are often used in conditional statements (if, while, etc.) and boolean expressions to control the flow of a program.
    
    Types of Logical Operators:
    AND (&&): Returns true if both operands are true.
    OR (||): Returns true if at least one of the operands is true.
    NOT (!): Negates the boolean value of its operand.
    
    Logical Operators
    Logical operators are fundamental components of programming languages that allow you to combine multiple conditions and make decisions based on the result. They are often used in conditional statements (if, while, etc.) and boolean expressions to control the flow of a program.
    
    Types of Logical Operators:
    AND (&&): Returns true if both operands are true.
    OR (||): Returns true if at least one of the operands is true.
    NOT (!): Negates the boolean value of its operand.

---
+++ logical operator/log.js
@@ -0,0 +1,46 @@
+//LOGICAL OPERATORS
+// These operator perform the logic.
+//There are three main types of logical operator
+// (i)And Operator  (&&)
+// (ii)Or Operator  (||)
+// (iii)Not Operator (!())
+//AND OPERATOR
+//  "and" operator  check two condition if both condition are true so it log true answer or else it log false
+var num1 = 21;
+var num2 = 22;
+console.log(num1 == 21 && num2 == 22);
+var num5 = 25;
+var num6 = 26;
+var num7 = 27;
+console.log(num5 < num6 && num7 == num5);
+var Num8 = 28;
+var num9 = 29;
+console.log(num9 <= Num8 && Num8 != num9);
+var num10 = 30;
+var num11 = 31;
+console.log(num10 < num11 && num11 == 31);
+var num12 = 32;
+var num13 = 33;
+console.log(num12 <= num13 && num13 == 33);
+//OR OPERATOR
+//"or" operator check two conditions if one conditions is true and the second condition is false so it log true answer
+var num3 = 23;
+var num4 = 24;
+console.log(num3 === 23 || num4 === 25);
+var num14 = 34;
+var num15 = 35;
+var name1 = "Afshan";
+var name2 = "feroz";
+var num16 = 36;
+var num17 = 37;
+console.log(num14 != 34 || num15 == 35);
+console.log(name2 == "feroz" || name2 != "afshan");
+console.log(num16 == 37 || num17 == 36);
+//NOT OPERATOR
+//"Not" operator log answer true if the answer is right.
+var num18 = 38;
+var num19 = 39;
+var num20 = 40;
+console.log(!(num18 !== 38));
+console.log(!(num19 == 39));
+console.log(!(num20 == 45));
---
+++ logical operator/log.ts
@@ -0,0 +1,62 @@
+//LOGICAL OPERATORS
+// These operator perform the logic.
+//There are three main types of logical operator
+// (i)And Operator  (&&)
+// (ii)Or Operator  (||)
+// (iii)Not Operator (!())
+
+
+//AND OPERATOR
+//  "and" operator  check two condition if both condition are true so it log true answer or else it log false
+
+let num1:number=21;
+let num2:number=22;
+console.log(num1==21 && num2==22);
+
+let num5=25
+let num6=26
+let num7=27
+console.log(num5<num6 && num7==num5)
+
+const Num8=28
+let num9=29
+console.log(num9<=Num8 && Num8!=num9)
+
+var num10=30
+var num11=31
+console.log(num10<num11 && num11==31)
+ 
+const num12=32
+const num13=33
+console.log(num12<=num13 && num13==33)
+//OR OPERATOR
+//"or" operator check two conditions if one conditions is true and the second condition is false so it log true answer
+
+ const num3:number=23
+ const num4:number=24
+ console.log(num3===23 || num4===25)
+
+ var num14:number=34
+ var num15:number=35
+
+ var name1="Afshan"
+ var name2="feroz"
+
+ var num16=36
+ var num17=37
+
+ console.log(num14!=34 || num15==35)
+ console.log(name2=="feroz" || name2!="afshan")
+ console.log(num16==37 || num17==36)
+
+ //NOT OPERATOR
+//"Not" operator log answer true if the answer is right.
+
+let num18:number=38
+let num19:number=39
+let num20:number=40
+
+
+console.log(!(num18!==38))
+console.log(!(num19==39))
+console.log(!(num20==45))
